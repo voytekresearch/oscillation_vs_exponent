416da90465cfa1612125199d012730fdbf713db9
Michael <mpreston@ucsd.edu>
2023-01-31 15:29:26 -0800

    feat: update setp 5 for all trials

--- code/5_prep_ols_data.py
+++ code/5_prep_ols_data.py
@@ -6,10 +6,10 @@ Created on Wed Sep 22 11:46:53 2021
 """
 
 # ===== Imports =====
-from os.path import join, exists
-from os import makedirs
+import os
 import numpy as np
 import pandas as pd
+
 from fooof import FOOOFGroup
 from fooof.bands import Bands
 from fooof.analysis import get_band_peak_fg
@@ -17,13 +17,13 @@ from fooof.utils import trim_spectrum
 
 
 # ===== parameters =====
-# project params
+# = project params =
 
 # paths and filenames
 PROJECT_PATH = 'C:/Users/micha/projects/oscillation_vs_exponent/'
-DIR_FIG = join(PROJECT_PATH, 'figures/manuscript_figures')
-DIR_STATS = join(PROJECT_PATH, 'data/ieeg_stats/permutation_test')
-FNAME_STATS = join(PROJECT_PATH, 'data/fellner_stats/fellner_stats_df.pkl')
+DIR_FIG = f"{PROJECT_PATH}/figures/manuscript_figures"
+DIR_STATS = f"{PROJECT_PATH}/data/ieeg_stats/permutation_test"
+FNAME_STATS = f"{PROJECT_PATH}/data/fellner_stats/fellner_stats_df.pkl"
 
 # dataset details
 FS = 512 # sampling frequency
@@ -31,8 +31,7 @@ TMIN = -1.5 # epoch start time
 PATIENTS = ['pat02','pat04','pat05','pat08','pat10','pat11','pat15','pat16',
             'pat17','pat19','pat20','pat21','pat22'] # subject IDs
 
-# spectral analysis parameters
-
+# = spectral analysis parameters =
 # define spectral bands of interest
 bands = Bands({'alpha' : [8, 20]})
 
@@ -44,32 +43,42 @@ SIG_LEVEL = 0.01
 
 def main():
     # id directories
-    dir_output = join(PROJECT_PATH, 'data/results')
-    if not exists(dir_output):
-        makedirs(dir_output)
+    dir_output = f"{PROJECT_PATH}/data/results"
+    if not os.path.exists(dir_output):
+        os.makedirs(dir_output)
 
     # Identify channels with reported effects (Fellner, 2019) 
     df_stats = np.load(FNAME_STATS, allow_pickle=True)
     sig_chans = df_stats['h_cond_alphabeta'] | df_stats['h_cond_gamma']
 
     # load channel info
-    chan_info = pd.read_pickle(join(PROJECT_PATH, 'data/ieeg_metadata', 
-                                'ieeg_channel_info.pkl'))
+    chan_info = pd.read_pickle(F"{PROJECT_PATH}/data/ieeg_metadata/ieeg_channel_info.pkl")
 
     # get data for each parameter and condition (sig chans only)
-    df_list = []
-    for material in ['word', 'face']:
-        df_list.append(gen_df_alpha(chan_info, material, AP_MODE, SIG_LEVEL)[sig_chans])
-        df_list.append(gen_df_exp(chan_info, material, AP_MODE, SIG_LEVEL)[sig_chans])
+    dfs = []
+    for memory in ['hit', 'miss']:
+        df_list = []
+        for material in ['words', 'faces']:
+            df_list.append(gen_df_alpha(chan_info, material, memory, AP_MODE)[sig_chans])
+            df_list.append(gen_df_exp(chan_info, material, memory, AP_MODE, SIG_LEVEL)[sig_chans])
+            
+        # aggregate data for all params and conditions 
+        df_mem = gen_df_ols(df_list[0],df_list[1],df_list[2],df_list[3], memory, sig_chans)
+
+        # add column for memory condition
+        df_mem['memory'] = memory
+
+        # add to list of dataframes
+        dfs.append(df_mem)
+
+    # join dataframes for hit and miss conditions
+    df_ols = pd.concat(dfs, ignore_index=True)
         
-    # aggregate data for all params and conditions 
-    df_ols = gen_df_ols(df_list[0],df_list[1],df_list[2],df_list[3],sig_chans)
-    
     # save dataframe for OLS analysis
-    df_ols.to_csv(join(dir_output, 'df_ols.csv'))
+    df_ols.to_csv(f"{dir_output}/df_ols.csv")
     
     
-def gen_df_ols(df_alpha_w, df_exp_w, df_alpha_f, df_exp_f, sig_chans):
+def gen_df_ols(df_alpha_w, df_exp_w, df_alpha_f, df_exp_f, memory, sig_chans):
     # add alpha results
     df = df_alpha_w.copy()
     df['diff_alpha_w'] = df["power_diff"]
@@ -81,19 +90,20 @@ def gen_df_ols(df_alpha_w, df_exp_w, df_alpha_f, df_exp_f, sig_chans):
     df['diff_exp_f'] = df_exp_f['exp_diff']
     
     # add intersection freqeuncy results
-    data_in_word = np.load(join(PROJECT_PATH,'data/ieeg_intersection_results/from_params', 
-                                 'intersection_results_%s_%s.npz' %('word', AP_MODE)), allow_pickle=True)
-    data_in_face = np.load(join(PROJECT_PATH,'data/ieeg_intersection_results/from_params', 
-                                 'intersection_results_%s_%s.npz' %('face', AP_MODE)), allow_pickle=True)
+    fname_in = f"intersection_results_words_{memory}_{AP_MODE}.npz"
+    data_in_word = np.load(f"{PROJECT_PATH}/data/ieeg_intersection_results/{fname_in}", allow_pickle=True)
+    fname_in = f"intersection_results_faces_{memory}_{AP_MODE}.npz"
+    data_in_face = np.load(f"{PROJECT_PATH}/data/ieeg_intersection_results/{fname_in}", allow_pickle=True)
+
     df['f_rot_w'] = data_in_word['intersection'][sig_chans]
     df['f_rot_f'] = data_in_face['intersection'][sig_chans]
     
     # add alpha/beta bandpower results
     # laod psd results
-    psd_pre_word = np.load(PROJECT_PATH + 'data/ieeg_spectral_results/' + 'psd_words_hit_prestim.npz')
-    psd_post_word = np.load(PROJECT_PATH + 'data/ieeg_spectral_results/' + 'psd_words_hit_poststim.npz')
-    psd_pre_face = np.load(PROJECT_PATH + 'data/ieeg_spectral_results/' + 'psd_faces_hit_prestim.npz')
-    psd_post_face = np.load(PROJECT_PATH + 'data/ieeg_spectral_results/' + 'psd_faces_hit_poststim.npz')
+    psd_pre_word = np.load(f"{PROJECT_PATH}/data/ieeg_spectral_results/psd_words_hit_prestim.npz")
+    psd_post_word = np.load(f"{PROJECT_PATH}/data/ieeg_spectral_results/psd_words_hit_poststim.npz")
+    psd_pre_face = np.load(f"{PROJECT_PATH}/data/ieeg_spectral_results/psd_faces_hit_prestim.npz")
+    psd_post_face = np.load(f"{PROJECT_PATH}/data/ieeg_spectral_results/psd_faces_hit_poststim.npz")
     freq = psd_post_face['freq']
     diff_psd_word = np.log10(psd_post_word['spectra']) - np.log10(psd_pre_word['spectra'])
     diff_psd_face = np.log10(psd_post_face['spectra']) - np.log10(psd_pre_face['spectra'])
@@ -113,39 +123,39 @@ def gen_df_ols(df_alpha_w, df_exp_w, df_alpha_f, df_exp_f, sig_chans):
     
     return df
 
-def gen_df_exp(chan_info, material, ap_mode, sig_level):
+def gen_df_exp(chan_info, material, memory, ap_mode, sig_level):
     # initialize dataframe
     df = chan_info.copy()
 
     # add exponent results for each epoch
-    for epoch in ['pre', 'post']:
+    for epoch in ['prestim', 'poststim']:
         # load spectral results
         param = FOOOFGroup()
-        param.load(join(PROJECT_PATH, 'data/ieeg_psd_param', 
-                        'psd_%s_%s_params_%s.json' %(epoch, material, ap_mode)))
+        fname_in = f"{material}_{memory}_{epoch}_params_{ap_mode}.json"
+        param.load(f"{PROJECT_PATH}/data/ieeg_psd_param/{fname_in}")
     
         # aggregate in dataframe with chan info
-        df['exp_%s' %epoch] = param.get_params('aperiodic','exponent')
+        df[f"exp_{epoch.replace('stim','')}"] = param.get_params('aperiodic','exponent')
 
     # compute change in exponent
     df['exp_diff'] = df['exp_post'] - df['exp_pre']
     
     return df
 
-def gen_df_alpha(chan_info, material, ap_mode):
+def gen_df_alpha(chan_info, material, memory, ap_mode):
     # initialize dataframe
     df_alpha = chan_info.copy()
 
     # add alpha peak results for each epoch
-    for epoch in ['pre', 'post']:
+    for epoch in ['prestim', 'poststim']:
         # load spectral results
         param = FOOOFGroup()
-        param.load(join(PROJECT_PATH, 'data/ieeg_psd_param',
-                        'psd_%s_%s_params_%s.json' %(epoch, material, ap_mode)))
+        fname_in = f"{material}_{memory}_{epoch}_params_{ap_mode}.json"
+        param.load(f"{PROJECT_PATH}/data/ieeg_psd_param/{fname_in}")
         
         # get alpha peak info and add to dataframe
         alpha = get_band_peak_fg(param, [8, 20])
-        df_alpha['power_%s' %epoch] = alpha['power']
+        df_alpha[f"power_{epoch.replace('stim','')}"] = alpha[:,1]
         
     # compute change in alpha
     df_alpha['power_diff'] = df_alpha['power_post'] - df_alpha['power_pre']
