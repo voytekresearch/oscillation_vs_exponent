ef4b3670fb30d4000a4a132d12b8777f7b25d191
Michael <mpreston@ucsd.edu>
2023-02-14 10:42:27 -0800

    feat: output data as csv

--- code/6_permutation_stats.py
+++ code/6_permutation_stats.py
@@ -47,6 +47,9 @@ def main():
     dir_output = f'{PROJECT_PATH}/data/ieeg_stats/permutation_test'
     if not os.path.exists(dir_output): os.makedirs(dir_output)
     
+    # initialize 
+    dfs = []
+
     # load channel info
     chan_info = np.load(f'{PROJECT_PATH}/data/ieeg_metadata/ieeg_channel_info.pkl',
         allow_pickle=True)
@@ -79,7 +82,7 @@ def main():
         for i_pat, patient in enumerate(PATS):
             # display progress
             print('Analyzing: %s - %s' %(patient, material))
-            
+
             # load subject data (pre/post-stim PSDs)
             fname = '%s_%ss_hit_xxx_psd.npz' %(patient, material)
             data_pre = np.load(f"{dir_input}/{fname.replace('xxx','prestim')}")
@@ -88,25 +91,23 @@ def main():
             # run permutation stats
             exp_diff_pat = exp_diff[chan_info['patient']==patient]
             alpha_diff_pat = alpha_diff[chan_info['patient']==patient]
-            results = resampling_analysis(data_pre['freq'], data_pre['psd'], 
+            df_i = resampling_analysis(data_pre['freq'], data_pre['psd'], 
                                           data_post['psd'], exp_diff_pat,
                                           alpha_diff_pat, AP_MODE, BANDS,
                                           n_iterations=N_ITER, n_jobs=N_JOBS)
 
-            pval_exp, sign_exp, distr_exp, \
-            pval_alpha, sign_alpha, distr_alpha = results
+            # aggregate
+            df_i['patient'] = patient
+            df_i['material'] = material
+            dfs.append(df_i)
             
-            # save results
-            # fname_out = '%s_%s_%s_diff_exp_randperm' %(patient, material, ap_mode)
-            fname_out = f'{patient}_{material}_{AP_MODE}_permutest'
-            np.savez(f"{dir_output}/{fname_out}", pval_exp=pval_exp, 
-                     sign_exp=sign_exp, distr_exp=distr_exp, 
-                     diff_exp=exp_diff_pat, pval_alpha=pval_alpha, 
-                     sign_alpha=sign_alpha, diff_alpha=alpha_diff_pat)
+            # save results for patient-material
+            fname_out = f'\stats_{patient}_{material}_{AP_MODE}'
+            df.to_csv(f"{dir_output}/{fname_out}.csv")
             
-    # aggregate statistical results for each condition
-    aggregate_stats_by_condition(dir_output)    
-
+    # aggregate statistical results and save
+    df = pd.concat(dfs)
+    df.to_csv(f"{dir_output}/stats_all.csv")
         
 def resampling_analysis(freq, spectra_pre, spectra_post, exp_diff, alpha_diff, 
                         ap_mode, bands, n_iterations=1000, n_jobs=1):
@@ -141,12 +142,20 @@ def resampling_analysis(freq, spectra_pre, spectra_post, exp_diff, alpha_diff,
         pval_exp[i_chan], sign_exp[i_chan] = comp_resampling_pval(distr_exp[i_chan], 
                                                            exp_diff[i_chan])
         pval_alpha[i_chan], sign_alpha[i_chan] = comp_resampling_pval(distr_alpha[i_chan], 
-                                                           alpha_diff[i_chan])        
+                                                           alpha_diff[i_chan])
+
+        # create dataframe of reults
+        df = pd.DataFrame({ 'pval_exp' : pval_exp, 
+                            'sign_exp' : sign_exp,
+                            'pval_alpha' : pval_alpha,
+                            'sign_alpha' : sign_alpha})
+        df['chan_idx'] = i_chan
+
         # time it
         end = timer()
         print('channel %d / %d: %0.1f seconds' %(i_chan+1, n_chans, end-start))
         
-    return pval_exp, sign_exp, distr_exp, pval_alpha, sign_alpha, distr_alpha
+    return df
     
 def shuffle_spectra(spectra_0, spectra_1, order):
     # concatenate 2 groups of spectra
@@ -183,29 +192,6 @@ def calc_param_change(freq, spectra_0, spectra_1, ap_mode, bands, n_jobs=1):
     alpha_diff = alpha_1[:,1] - alpha_0[:,1] 
     
     return exp_diff, alpha_diff
-
-def aggregate_stats_by_condition(path_out):
-# load stats and aggregate group results
-
-    # loop through both materials
-    for material in ['word', 'face']:
-        # initialize
-        pval_exp = []
-        pval_alpha = []
-        sign_exp = []
-        sign_alpha = []
-        
-        for patient in PATS:
-            fname_in = f"{path_out}/{patient}_{material}_{AP_MODE}_permutest.npz"
-            data_in = np.load(fname_in)
-            pval_alpha = np.append(pval_alpha, data_in['pval_alpha'])
-            sign_alpha = np.append(sign_alpha, data_in['sign_alpha'])
-            pval_exp = np.append(pval_exp, data_in['pval_exp'])
-            sign_exp = np.append(sign_exp, data_in['sign_exp'])
-        
-        np.savez(f"{path_out}/group_stats_{material}_{AP_MODE}",
-                pval_alpha=pval_alpha, pval_exp=pval_exp, sign_exp=sign_exp,
-                sign_alpha=sign_alpha)
         
 
 if __name__ == "__main__":
