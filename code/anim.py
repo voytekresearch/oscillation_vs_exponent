7f9541f1bef47769db724e3ca3e1d0fbef0a3482
Michael <mpreston@ucsd.edu>
2023-02-16 12:39:29 -0800

    feat: create template animation module

---
+++ code/anim.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Animation utility functions
+"""
+
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib as mpl
+from matplotlib import rcParams
+
