fe26480f3b05b5fbb10c38b99438560d5e279e6b
Michael <mpreston@ucsd.edu>
2023-02-23 13:53:32 -0800

    chore: remove optimization analysis

--- code/4_compute_intersection.py
+++ code/4_compute_intersection.py
@@ -16,12 +16,9 @@ ieeg_3_spectral_parameterization.py
 from os.path import join, exists
 from os import mkdir
 import numpy as np
-from scipy.optimize import  minimize, Bounds
-from scipy.stats import mode
 
 from fooof import FOOOFGroup
 from fooof.sim import gen_power_spectrum
-from neurodsp.spectral import rotate_powerlaw
 
 # Parameters
 PROJECT_PATH = 'C:/Users/micha/projects/oscillation_vs_exponent/'
@@ -51,18 +48,15 @@ def main():
         
                 # calc intersection 
                 psd_pre, psd_post, intersection, intersection_idx = \
-                    comp_intersection_from_params(param_pre, param_post)
+                    comp_intersection(param_pre, param_post)
                 
-                # # calc intersection using optimization
-                # psd_pre, psd_post, intersection = solve_for_rotation_freq(N_ITER, param_pre, param_post, average=AVERAGE_METHOD)
-        
                 # save results
                 fname_out = 'intersection_results_%s_%s_%s' %(material, memory, ap_mode)
                 np.savez(join(dir_output, fname_out), psd_pre=psd_pre, psd_post=psd_post, 
                         intersection=intersection)
         
 
-def comp_intersection_from_params(param_pre, param_post):
+def comp_intersection(param_pre, param_post):
     """ 
     Calculate intersection of pre and post stim psd
 
@@ -136,88 +130,6 @@ def comp_intersection_from_params(param_pre, param_post):
         
     return psd_pre, psd_post, intersection, intersection_idx
 
-def rotate_psd(f_rotation, freqs, psd_0, psd_1, delta_exp):
-    """
-    Rotate power spectrum and compute the difference betweeen the rotated spectrum and a 
-    second spectrum. This function is used to minimize the difference between the rotated
-    and second spectrum. Spectra are log-transformed before teh difference is computed.
-    The error between the two spectra is squared.
-
-    Parameters
-    ----------
-    f_rotation : float
-        frequency at which to rotate the power spectrum
-    freqs : 1d array
-        frequency vector
-    psd_0 : 1d array
-        power spectrum to rotate
-    psd_1 : 1d array
-        power spectrum to compare rotated spectrum to
-    delta_exp : float
-        degree of rotation (change in exponent)
-
-    Returns
-    -------
-    diff : float
-        difference between rotated and second spectrum (squared error)
-
-    """
-
-    # rotate
-    psd_rot = rotate_powerlaw(freqs, psd_0, delta_exponent=delta_exp, f_rotation=f_rotation)
-    
-    # calc difference in power between parameter sets
-    diff = np.sum((np.log10(psd_1) - np.log10(psd_rot))**2)
-    
-    return diff
-
-def minimize_rotation_error(param_0, param_1, x0):
-    # cont number of spectra
-    n_spectra = len(param_0.get_params('r_squared'))
-    
-    # calc change/difference in exponent
-    delta_exp = param_1.get_params('aperiodic', 'exponent') - param_0.get_params('aperiodic', 'exponent')
-    
-    # initialize array for results
-    psd_0 = np.zeros([n_spectra, len(param_0.freqs)])
-    psd_1 = psd_0.copy()
-    intersection = np.zeros(n_spectra)
-    intersection[:] = np.nan
-    # loop through spectra
-    for ii in range(n_spectra):
-        # generate aperiodic spectra
-        psd_freq, psd_0[ii] = gen_power_spectrum(param_0.freq_range, param_0.get_params('aperiodic')[ii], [], freq_res=param_0.freq_res)
-        _, psd_1[ii] = gen_power_spectrum(param_1.freq_range, param_1.get_params('aperiodic')[ii], [], freq_res=param_1.freq_res)
-        
-        # find intersection
-        result = minimize(rotate_psd, x0=x0, bounds=Bounds(lb=[param_0.freqs.min(),], ub=[param_0.freqs.max(),]),
-                          args=(psd_freq, psd_0[ii], psd_1[ii], delta_exp[ii]))
-        intersection[ii] = result['x']
-
-    return psd_0, psd_1, intersection
-
-def solve_for_rotation_freq(n_iter, param_0, param_1, average='mode'):
-    
-    # initialize output array
-    intersection_rand = np.zeros([n_iter, len(param_0)])
-    
-    # solve for intersection
-    for i_iter in range(N_ITER):
-        x0 = param_0.freqs[np.random.randint(0, len(param_0.freqs))]
-        psd_0, psd_1, intersection_rand[i_iter] = minimize_rotation_error(param_0, param_1, x0=[x0,])
-    
-    # take average over iterations
-    if average == 'median':
-        f_rot = np.nanmedian(intersection_rand, 0)
-    elif average == 'mean':
-        f_rot = np.nanmean(intersection_rand, 0)
-    elif average == 'mode':
-        # round solutions to nearest whole number
-        # final solution must have a count > n_iter / 2
-        f_rot, count = mode(np.round_(intersection_rand), 0)
-        f_rot[count < n_iter/2] = np.nan
-    
-    return psd_0, psd_1, f_rot
     
 if __name__ == "__main__":
     main()
