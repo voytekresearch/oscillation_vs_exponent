25637f912438766c546acfd683eb3a02cc349f3f
Michael <mpreston@ucsd.edu>
2023-04-18 18:34:15 -0700

    chore: rm group tfr func

--- code/3_spectral_parameterization.py
+++ code/3_spectral_parameterization.py
@@ -53,7 +53,6 @@ def main():
     if RUN_TFR:
         print('\nParameterizing TFRs...')
         parameterize_tfr()
-        # param_group_tfr_results() # computationally intensive
 
 def param_group_psd_results():
     # identify / create directories
@@ -97,52 +96,6 @@ def param_group_psd_results():
     # display progress
     hour, min, sec = hour_min_sec(timer() - t_start)
     print(f"Total PSD analysis time: {hour} hour, {min} min, and {sec :0.1f} s")
-
-def param_group_tfr_results():
-    # time it
-    t_start = timer()
-
-    # identify / create directories
-    dir_input = f"{PROJECT_PATH}/data/ieeg_spectral_results"
-    dir_output = f"{PROJECT_PATH}/data/ieeg_tfr_param"
-    if not os.path.exists(dir_output): 
-        os.makedirs(f"{dir_output}/fooof_reports")
-        
-    # loop through conditions
-    conditions = ['words_hit', 'faces_hit','words_miss', 'faces_miss']
-    for cond in conditions:
-        # display progress
-        print(f"\tAnalyzing condition: \t{cond.replace('_', ', ')}...")
-        t_start_c = timer()
-    
-        # load group TFR results
-        data_in = np.load(f"{dir_input}/tfr_{cond}.npz")
-        freq = data_in['freq']
-        tfr_in = data_in['tfr']
-        
-        # reshape TFR for parameterization
-        tfr = tfr_in.reshape([tfr_in.shape[0]*tfr_in.shape[1],tfr_in.shape[2]])
-        
-        # parameterize (fit both with and without knee parametere)
-        for ap_mode in ['knee']: # ['fixed', 'knee']:
-            # print(f"\t\tParameterizing with '{ap_mode}' aperiodic mode...")
-            fg = FOOOFGroup(**SPEC_PARAM_SETTINGS, aperiodic_mode=ap_mode, verbose=False)
-            fg.set_check_data_mode(False)
-            fg.fit(freq, tfr, n_jobs=N_JOBS, freq_range=FREQ_RANGE)
-            
-            # save results and report
-            fname_out = 'tfr_%s_params_%s' %(cond, ap_mode)
-            fg.save(f"{dir_output}/{fname_out}", save_results=True, 
-                    save_settings=True, save_data=True)
-            fg.save_report(f"{dir_output}/fooof_reports/{fname_out}")
-
-        # display progress
-        hour, min, sec = hour_min_sec(timer() - t_start_c)
-        print(f"\t\tCondition ompleted in {hour} hour, {min} min, and {sec :0.1f} s")
-
-    # display progress
-    hour, min, sec = hour_min_sec(timer() - t_start)
-    print(f"Total TFR analysis time: {hour} hour, {min} min, and {sec :0.1f} s")
  
 
 def parameterize_tfr():
