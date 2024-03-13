fff6d9f295baade719e51bae05b225467ed6fe12
Michael <mpreston@ucsd.edu>
2024-03-12 20:47:18 -0700

    add MATERIALS

--- code/info.py
+++ code/info.py
@@ -6,3 +6,4 @@ PATIENTS = ['pat02','pat04','pat05','pat08','pat10','pat11','pat15','pat16',
             'pat17','pat19','pat20','pat21','pat22']
 FS = 512 # iEEG sampling frequency
 TMIN = -1.5 # epoch start time
+MATERIALS = ['words', 'faces']
