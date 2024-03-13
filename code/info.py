1a20f4f17d850ac85527ab8f2c90f73de8fd7988
Michael <mpreston@ucsd.edu>
2024-03-12 21:00:08 -0700

    add MEMORY

--- code/info.py
+++ code/info.py
@@ -6,4 +6,5 @@ PATIENTS = ['pat02','pat04','pat05','pat08','pat10','pat11','pat15','pat16',
             'pat17','pat19','pat20','pat21','pat22']
 FS = 512 # iEEG sampling frequency
 TMIN = -1.5 # epoch start time
-MATERIALS = ['words', 'faces']
+MATERIALS = ['words', 'faces'] # experimental blocks (stimulus type) 
+MEMORY = ['hit', 'miss'] # behavior (memory performance)
