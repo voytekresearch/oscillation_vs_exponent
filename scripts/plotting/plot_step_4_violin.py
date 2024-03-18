2658c7bc29f33a42b630096b955f76a1b674536b
Michael <mpreston@ucsd.edu>
2024-03-18 11:49:27 -0700

    add plot_step_4_violin

---
+++ scripts/plotting/plot_step_4_violin.py
@@ -0,0 +1,179 @@
+"""
+Plot results of scripts/step4_spectral_parameterization.py. For each feature of
+interest, plot the distribution of values for the baseline and encoding period
+as a violin and swarm plot, with 'split' and 'dodge' set to True. Also plot the
+distribution of change in each feature between the baseline and encoding periods
+as a histogram below the violin plot. 
+
+"""
+
+# Imports - standard
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+import seaborn as sns
+
+# Imports - custom
+import sys
+sys.path.append("code")
+from paths import PROJECT_PATH
+from info import MATERIALS
+from utils import get_start_time, print_time_elapsed
+from plots import set_custom_colors
+from specparam_utils import knee_freq
+
+# settings
+plt.style.use('mplstyle/default.mplstyle')
+FEATURES = ['offset', 'knee', 'exponent', 'alpha', 'alpha_adj', 'gamma',
+            'gamma_adj']
+TITLES = ['Aperiodic offset', 'Aperiodic knee', 'Aperiodic exponent', 
+          'Total alpha power', 'Adjusted alpha power', 'Total gamma power', 
+            'Adjusted gamma power']
+LABELS = ['offset (a.u.)', 'knee (Hz)', 'exponent', 'power (a.u.)',
+            'power (a.u.)', 'power (a.u.)', 'power (a.u.)']
+
+
+def main():
+
+    # display progress
+    t_start = get_start_time()
+
+    # identify / create directories
+    dir_output = f"{PROJECT_PATH}/figures/param_violin"
+    if not os.path.exists(dir_output): 
+        os.makedirs(f"{dir_output}")
+
+    # load SpecParam results
+    fname = r"C:\Users\micha\projects\oscillation_vs_exponent\data\results\spectral_parameters.csv"
+    df = pd.read_csv(fname, index_col=0)
+    df = df.loc[df['memory']=='hit'].reset_index(drop=True)
+
+    # get results for task-modulated channels
+    fname = f"{PROJECT_PATH}/data/results/ieeg_modulated_channels.csv"
+    stats = pd.read_csv(fname, index_col=0)
+    df = df.merge(stats, on=['patient', 'chan_idx'])
+    df = df.loc[df['sig_all']].reset_index(drop=True)
+
+    # convert knee to Hz
+    df['knee'] = knee_freq(df['knee'], df['exponent'])
+
+    # plot
+    for feature, title, label in zip(FEATURES, TITLES, LABELS):
+        fname_out = f"param_violin_{feature}.png"
+        plot_contrasts_violin(df, feature, title=title, y_label=label,
+                                fname_out=f"{dir_output}/{fname_out}")
+
+    # display progress
+    print(f"\n\nTotal analysis time:")
+    print_time_elapsed(t_start)
+
+
+def plot_contrasts_violin(df, y_var, title='', y_label=None, fname_out=None):
+    # plot in each color
+    set_custom_colors('browns')
+    _plot_contrasts_violin(df, y_var, title=title, y_label=y_label, loc='left',
+                           fname_out=fname_out.replace('.png', '_browns.png'))
+    set_custom_colors('blues')
+    _plot_contrasts_violin(df, y_var, title=title, y_label=y_label, loc='right',
+                           fname_out=fname_out.replace('.png', '_blues.png'))
+
+    # load each, crop, and join
+    img_0 = plt.imread(fname_out.replace('.png', '_browns.png'))
+    img_1 = plt.imread(fname_out.replace('.png', '_blues.png'))
+    idx_mid = img_0.shape[1] // 2
+    image = np.concatenate([img_0[:, :idx_mid], img_1[:, idx_mid:]], axis=1)
+
+    # plot combined image
+    fig, ax = plt.subplots(1,1)
+    ax.imshow(image)
+    ax.axis('off')
+
+    # save
+    if fname_out:
+        plt.savefig(fname_out)
+        plt.close()
+    else:
+        plt.show()
+        
+        
+def _plot_contrasts_violin(params, y_var, title='', y_label=None, 
+                          fname_out=None, plot_swarm=True, loc='left', 
+                          subplot_label=''):
+    # set plotting params
+    plotting_params = {
+        'data'  :   params,
+        'x'     :   'material',
+        'hue'   :   'epoch',
+        'y'     :   y_var,
+        'dodge' :   True,
+        'split' :   True
+    }
+
+    # init
+    if y_label is None:
+        y_label = y_var.lower()
+
+    # create figure
+    fig = plt.figure(figsize=[4, 6])
+    gs = fig.add_gridspec(2,2, height_ratios=[2,1])
+    ax1 = fig.add_subplot(gs[0,:])
+    ax2l = fig.add_subplot(gs[1,0])
+    ax2r = fig.add_subplot(gs[1,1])
+    
+    # ===== Upper Subplot =====
+    # plot violin and swarm
+    vp = sns.violinplot(**plotting_params, ax=ax1)
+    if plot_swarm:
+        plotting_params.pop('split')
+        sp = sns.swarmplot(**plotting_params, ax=ax1, size=2, 
+                           palette='dark:#000000')
+
+    # remove mean and quartile line
+    for l in ax1.lines:
+        l.set_linewidth(0)
+
+    # Label
+    ax1.set_title(title)
+    vp.set_xlabel('')
+    vp.set_ylabel(y_label)
+    vp.set_xticks([0,1], labels=['word block','face block'])
+    vp.xaxis.set_ticks_position('top')
+
+    # add legend (and add space)
+    handles, _ = vp.get_legend_handles_labels()
+    if loc == 'left':
+        vp.legend(handles=handles, labels=['baseline','encoding'],
+                  bbox_to_anchor=(0.05, -0.1, 0.2, 0.1))
+    elif loc == 'right':
+        vp.legend(handles=handles, labels=['baseline','encoding'],
+                  bbox_to_anchor=(0.82, -0.1, 0.2, 0.1))
+    
+    # ===== Lower Subplot =====
+    # plot disributions exponent change)
+    df_p = params.pivot_table(index=['patient', 'chan_idx', 'material'], 
+                              columns='epoch', values=y_var).reset_index()
+    df_p['diff'] = df_p['post'] - df_p['pre']
+    max_val = np.nanmax(np.abs(df_p['diff']))
+    bins = np.linspace(-max_val, max_val, 30)
+    for ax, material in zip([ax2l, ax2r], MATERIALS):
+        diff = df_p.loc[df_p['material']==material, 'diff']
+        ax.hist(diff, bins=bins, color='grey', label=material)
+        ax.set(xlim=[-max_val, max_val])
+        ax.set_xlabel(f"$\Delta$ {y_label}")
+        ax.set_ylabel('channel count')
+        ax.axvline(np.nanmean(diff), color='k', linestyle='--')
+
+    # add subplot labels
+    fig.text(0, 1.0, subplot_label, fontweight='bold')
+    
+    # # save figure
+    if fname_out: 
+        fig.savefig(fname_out)
+        
+    return vp
+
+
+if __name__ == "__main__":
+    main()
