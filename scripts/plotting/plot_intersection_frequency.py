3c0ced4a7e6984f57208080d0dcf81582d7f47f5
Michael <mpreston@ucsd.edu>
2024-03-25 12:58:50 -0700

    replace subplot a with mean spectra

--- scripts/plotting/plot_intersection_frequency.py
+++ scripts/plotting/plot_intersection_frequency.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 # Imports - custom
 import sys
@@ -16,6 +17,7 @@ from paths import PROJECT_PATH
 from utils import get_start_time, print_time_elapsed
 from info import MATERIALS
 from settings import COLORS
+from plots import plot_spectra_2conditions
 
 
 def main():
@@ -41,35 +43,56 @@ def main():
     f_rot_all = np.concatenate([intersection['words'], intersection['faces']])
     f_rot_sig = np.concatenate([intersection['words'][df['sig_all']], 
                                 intersection['faces'][df['sig_all']]])
+    
+    # compute confidence interval for intersection frequency
+    scale = np.nanstd(f_rot_sig)/np.sqrt(len(f_rot_sig))
+    ci = norm.interval(0.95, loc=np.nanmean(f_rot_sig), scale=scale)
+
+    # load spectal results
+    psd = dict()
+    for material in MATERIALS:
+        for epoch in ['pre','post']:
+            fname = f"psd_{material}_hit_{epoch}stim.npz"
+            data_in = np.load(f"{PROJECT_PATH}/data/ieeg_spectral_results/{fname}")
+            psd[(material, epoch)] = data_in['spectra'][df['sig_all']]
+    freq = data_in['freq']
+
+    # compute grand average spectra
+    spectra_pre = np.concatenate([psd[material, 'pre'] for material in MATERIALS])
+    spectra_post = np.concatenate([psd[material, 'post'] for material in MATERIALS])
 
     # create figure
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=[8, 4])
 
-    # subplot 1 =====
-    bin_edges = np.linspace(0,100,11)
-    ax1.hist(f_rot_all, bins=bin_edges, alpha=0.6, color='grey', label='all electrodes')
-    ax1.hist(f_rot_sig, bins=bin_edges, alpha=0.6, color='k', label='electrodes with\n reported effects')
-    ax1.legend()
+    # subplot 1: Grand average spectra =========================================
 
-    # subplot 2 =====
-    ax2.hist(intersection['words'][df['sig_all']], bins=bin_edges, alpha=0.8, label='word block', color=COLORS['brown'])
-    ax2.hist(intersection['faces'][df['sig_all']], bins=bin_edges, alpha=0.8, label='face block', color=COLORS['blue'], zorder=0)
-    ax2.axvline(np.nanmedian(f_rot_sig), linestyle='--', color='k',label='median', zorder=2)
+    # plot spectra
+    plot_spectra_2conditions(spectra_pre, spectra_post, freq, ax=ax1,
+                                color=[COLORS['light_brown'], COLORS['brown']])
+    ax1.set_xlim([4, 100]) # SpecParam fitting range
+
+    # annotate 95% confidence interval
+    ax1.axvspan(ci[0], ci[1], color='grey', alpha=0.3)
+
+    # subplot 2: histogram of intersection frequency ===========================
+    bin_edges = np.linspace(0,100,11)
+    ax2.hist(intersection['words'][df['sig_all']], bins=bin_edges, alpha=0.8, 
+             label='word block', color=COLORS['brown'])
+    ax2.hist(intersection['faces'][df['sig_all']], bins=bin_edges, 
+             alpha=0.8, label='face block', color=COLORS['blue'], zorder=0)
+    ax2.axvline(np.nanmedian(f_rot_sig), linestyle='--', color='k', 
+                label='median', zorder=2)
 
     # add legend
     order = [0,1,2]
     handles, labels = ax2.get_legend_handles_labels()
     ax2.legend([handles[idx] for idx in order],[labels[idx] for idx in order])
-
-    # axis labels
-    for ax in[ax1, ax2]:
-        ax.set_xlabel('intersection frequency (Hz)')
-        ax.set_ylabel('electrode count')
+    ax2.set_xlabel('intersection frequency (Hz)')
+    ax2.set_ylabel('electrode count')
 
     # add titles
-    # fig.suptitle('Intersection of Power Spectra: Baseline v. Encoding')
-    ax1.set_title('All Electrodes')
-    ax2.set_title('Task-modulated electrodes')
+    ax1.set_title('Grand average PSD')
+    ax2.set_title('Intersection frequency')
         
     # save fig
     fig.savefig(f"{dir_output}/intersection_freq_histograms.png")
