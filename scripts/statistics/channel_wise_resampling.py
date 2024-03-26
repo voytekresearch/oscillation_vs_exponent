7a7294471289471b1e275e79ab9363dd29cff99a
Michael <mpreston@ucsd.edu>
2024-03-26 10:22:18 -0700

    feat: add gamma analysis. general update

--- scripts/statistics/channel_wise_resampling.py
+++ scripts/statistics/channel_wise_resampling.py
@@ -11,8 +11,7 @@ import pandas as pd
 from timeit import default_timer as timer
 
 from specparam import SpectralGroupModel
-from specparam.bands import Bands
-from specparam.analysis import get_band_peak
+from specparam.analysis import get_band_peak_group
 
 # Imports - custom
 import sys
@@ -38,8 +37,7 @@ def main():
     dfs = []
 
     # load channel info
-    fname_in = f'{PROJECT_PATH}/data/ieeg_metadata/ieeg_channel_info.pkl'
-    chan_info = np.load(fname_in, allow_pickle=True)
+    chan_info = pd.read_csv(f'{PROJECT_PATH}/data/ieeg_metadata/ieeg_channel_info.csv')
     
     # loop through materials
     for material in ['face','word']:
@@ -52,18 +50,21 @@ def main():
             
             # load spectral parameterization results
             param_pre = SpectralGroupModel()
-            param_pre.load(f"{PROJECT_PATH}/data/ieeg_psd_param/{material}s_{memory}_prestim_params_{AP_MODE}.json")
+            param_pre.load(f"{PROJECT_PATH}/data/ieeg_psd_param/psd_{material}s_{memory}_prestim_params_{AP_MODE}.json")
             param_post = SpectralGroupModel()
-            param_post.load(f"{PROJECT_PATH}/data/ieeg_psd_param/{material}s_{memory}_poststim_params_{AP_MODE}.json")
+            param_post.load(f"{PROJECT_PATH}/data/ieeg_psd_param/psd_{material}s_{memory}_poststim_params_{AP_MODE}.json")
             
-            # change NaN to 0 (no detectable alpha peak)
-            alpha_pre = get_band_peak(param_pre, BANDS['alpha'])
-            alpha_post = get_band_peak(param_post, BANDS['alpha'])
+            # Get band peaks for alpha and gamma
+            alpha_pre = get_band_peak_group(param_pre, BANDS['alpha'])
+            alpha_post = get_band_peak_group(param_post, BANDS['alpha'])
+            gamma_pre = get_band_peak_group(param_pre, BANDS['gamma'])
+            gamma_post = get_band_peak_group(param_post, BANDS['gamma'])
             
             # calc change in parameters (exponent and adjusted alpha power)
             exp_diff = param_post.get_params('aperiodic','exponent') - \
                 param_pre.get_params('aperiodic','exponent')
-            alpha_diff = alpha_post[:,1] - alpha_pre[:,1] 
+            alpha_diff = alpha_post[:,1] - alpha_pre[:,1]
+            gamma_diff = gamma_post[:,1] - gamma_pre[:,1]
 
             # loop through patients
             for patient in PATIENTS:
@@ -86,22 +87,21 @@ def main():
                 # get parameters for this patient
                 exp_diff_pat = exp_diff[chan_info['patient']==patient]
                 alpha_diff_pat = alpha_diff[chan_info['patient']==patient]
+                gamma_diff_pat = gamma_diff[chan_info['patient']==patient]
 
                 # run permutation stats
                 df_i = resampling_analysis(data_pre['freq'], data_pre['psd'], 
                                             data_post['psd'], exp_diff_pat,
-                                            alpha_diff_pat, AP_MODE, 
-                                            BANDS['alpha'], n_iterations=N_ITER, 
-                                            n_jobs=N_JOBS)
+                                            alpha_diff_pat, gamma_diff_pat)
 
                 # aggregate
-                df_i['patient'] = patient
-                df_i['material'] = material
-                df_i['memory'] = memory
+                df_i.insert(0, 'patient', patient)
+                df_i.insert(2, 'material', material)
+                df_i.insert(3, 'memory', memory)
                 dfs.append(df_i)
                 
                 # save results for patient-material-memory
-                fname_out = f'stats_{patient}_{material}_{memory}_{AP_MODE}.csv'
+                fname_out = f'stats_{patient}_{material}_{memory}.csv'
                 df_i.to_csv(f"{dir_output}/{fname_out}", index=False)
 
                 # display progress
@@ -117,7 +117,7 @@ def main():
     print(f'Total time: {timer() - start_time}')
         
 def resampling_analysis(freq, spectra_pre, spectra_post, exp_diff, alpha_diff, 
-                        ap_mode, f_range, n_iterations=1000, n_jobs=-1):
+                        gamma_diff):
     
     # size up data
     n_trials = spectra_pre.shape[0]
@@ -126,10 +126,13 @@ def resampling_analysis(freq, spectra_pre, spectra_post, exp_diff, alpha_diff,
     # initialize vars
     pval_exp = np.zeros(n_chans)
     sign_exp = np.zeros(n_chans)
-    distr_exp = np.zeros([n_chans, n_iterations])
+    distr_exp = np.zeros([n_chans, N_ITER])
     pval_alpha = np.zeros(n_chans)
     sign_alpha = np.zeros(n_chans)
-    distr_alpha = np.zeros([n_chans, n_iterations])
+    distr_alpha = np.zeros([n_chans, N_ITER])
+    pval_gamma = np.zeros(n_chans)
+    sign_gamma = np.zeros(n_chans)
+    distr_gamma = np.zeros([n_chans, N_ITER])
     
     # loop through channels
     for i_chan in range(n_chans):
@@ -139,38 +142,36 @@ def resampling_analysis(freq, spectra_pre, spectra_post, exp_diff, alpha_diff,
 
         
         # shuffle spectra
-        order = gen_random_order(n_iterations, int(n_trials*2))
+        order = gen_random_order(N_ITER, int(n_trials*2))
         spectra_0s, spectra_1s = shuffle_spectra(spectra_pre[:, i_chan], 
                                                  spectra_post[:, i_chan], order)
 
         # parameterize shuffled spectra and compute the difference in exponent
-        results = calc_param_change(freq, spectra_0s, spectra_1s, ap_mode, 
-                                    f_range, n_jobs=n_jobs)
-        distr_exp[i_chan], distr_alpha[i_chan] = results
+        results = calc_param_change(freq, spectra_0s, spectra_1s, AP_MODE)
+        distr_exp[i_chan], distr_alpha[i_chan], distr_gamma[i_chan] = results
 
         # compute p-value
-        temp = comp_resampling_pval(distr_exp[i_chan], exp_diff[i_chan])
-        pval_exp[i_chan], sign_exp[i_chan] = temp
+        pval_exp[i_chan] = comp_resampling_pval(distr_exp[i_chan], exp_diff[i_chan])
+        sign_exp[i_chan] = np.sign(exp_diff[i_chan])
 
-        temp = comp_resampling_pval(distr_alpha[i_chan], alpha_diff[i_chan])
-        pval_alpha[i_chan], sign_alpha[i_chan] = temp
-        
-        # check if all distr_alpha values are NaN
-        if np.isnan(distr_alpha[i_chan]).all():
-            pval_alpha[i_chan] = np.nan
-            sign_alpha[i_chan] = np.nan
-            print('\tall NaN values for alpha power distribution...')
+        pval_alpha[i_chan] = comp_resampling_pval(distr_alpha[i_chan], alpha_diff[i_chan])
+        sign_alpha[i_chan] = np.sign(alpha_diff[i_chan])
+
+        pval_gamma[i_chan] = comp_resampling_pval(distr_gamma[i_chan], gamma_diff[i_chan])
+        sign_gamma[i_chan] = np.sign(gamma_diff[i_chan])
 
         # time it
         end = timer()
         print(f"\tcomplete in: {end-start:0.0f} seconds")
 
     # create dataframe of reults
-    df = pd.DataFrame({ 'pval_exp' : pval_exp, 
+    df = pd.DataFrame({ 'chan_idx' : np.arange(n_chans),
+                        'pval_exp' : pval_exp, 
                         'sign_exp' : sign_exp,
                         'pval_alpha' : pval_alpha,
-                        'sign_alpha' : sign_alpha})
-    df['chan_idx'] = np.arange(n_chans)
+                        'sign_alpha' : sign_alpha,
+                        'pval_gamma' : pval_gamma,
+                        'sign_gamma' : sign_gamma})
         
     return df
     
@@ -188,26 +189,31 @@ def shuffle_spectra(spectra_0, spectra_1, order):
 
     return spectra_0s, spectra_1s
 
-def calc_param_change(freq, spectra_0, spectra_1, ap_mode, f_range, n_jobs=-1):
+def calc_param_change(freq, spectra_0, spectra_1, ap_mode):
     # initialize model
     sp_0 = SpectralGroupModel(**SPEC_PARAM_SETTINGS, aperiodic_mode=ap_mode, verbose=False)
     sp_0.set_check_data_mode(False)
     sp_1 = sp_0.copy()
 
     # fit
-    sp_0.fit(freq, spectra_0, n_jobs=n_jobs)
-    sp_1.fit(freq, spectra_1, n_jobs=n_jobs)
+    sp_0.fit(freq, spectra_0, n_jobs=N_JOBS)
+    sp_1.fit(freq, spectra_1, n_jobs=N_JOBS)
     
     # calc difference in exponent of shuffled spectra
     exp_diff = sp_1.get_params('aperiodic', 'exponent') - \
                sp_0.get_params('aperiodic', 'exponent')
     
     # calculate change in alpha amplitude
-    alpha_0 = get_band_peak(sp_0, f_range)
-    alpha_1 = get_band_peak(sp_1, f_range)
+    alpha_0 = get_band_peak_group(sp_0, BANDS['alpha'])
+    alpha_1 = get_band_peak_group(sp_1, BANDS['alpha'])
     alpha_diff = alpha_1[:,1] - alpha_0[:,1] 
+
+    # calculate change in gamma amplitude
+    gamma_0 = get_band_peak_group(sp_0, BANDS['gamma'])
+    gamma_1 = get_band_peak_group(sp_1, BANDS['gamma'])
+    gamma_diff = gamma_1[:,1] - gamma_0[:,1]
     
-    return exp_diff, alpha_diff
+    return exp_diff, alpha_diff, gamma_diff
         
 
 if __name__ == "__main__":
