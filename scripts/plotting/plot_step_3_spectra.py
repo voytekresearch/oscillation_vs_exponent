89f856a2a7931e616933d6b2762d33ae07c18036
Michael <mpreston@ucsd.edu>
2024-03-27 13:35:13 -0700

    combine materials into one plot

--- scripts/plotting/plot_step_3_spectra.py
+++ scripts/plotting/plot_step_3_spectra.py
@@ -16,11 +16,11 @@ sys.path.append("code")
 from paths import PROJECT_PATH
 from info import MATERIALS, MEMORY
 from plots import plot_spectra_2conditions
-from settings import COLORS
+from settings import COLORS, FREQ_RANGE
 
 # settings
 plt.style.use('mplstyle/default.mplstyle')
-FIGSIZE = [3, 2]
+FIGSIZE = [5, 2]
 
 
 def main():
@@ -34,35 +34,35 @@ def main():
     if not os.path.exists(f"{dir_fig}"): 
         os.makedirs(f"{dir_fig}")
 
-    for material in MATERIALS:
-        for memory in MEMORY:
-            for feature in ['alpha', 'gamma', 'all', 'any']:
-                plot_group_spectra(stats, material, memory, feature, dir_fig)
+    for feature in ['alpha', 'gamma', 'all', 'any']:
+        plot_group_spectra(stats, feature, dir_fig)
 
 
-def plot_group_spectra(stats, material, memory, feature, dir):
-    # load data
-    fname = f"{PROJECT_PATH}/data/ieeg_spectral_results/psd_{material}_{memory}_XXXstim.npz"
-    data_pre = np.load(fname.replace("XXX", "pre"))
-    data_post = np.load(fname.replace("XXX", "post"))
-    psd_pre = data_pre['spectra'][stats[f"sig_{feature}"]]
-    psd_post = data_post['spectra'][stats[f"sig_{feature}"]]
-    freq = data_pre['freq']
+def plot_group_spectra(stats, feature, dir):
 
-    # plot
-    if material=='words':
-        colors = [COLORS['light_brown'], COLORS['brown']]
-    elif material=='faces':
-        colors = [COLORS['light_blue'], COLORS['blue']]
+    # create figure
+    _, axes = plt.subplots(1, 2, figsize=FIGSIZE)
 
-    # title = f"{material} - {memory} - {feature}"
-    title = f"{material[0].upper()}{material[1:]}-encoding"
-    fname = f"{material}_{memory}_{feature}.png"
-    f_mask = np.logical_and(freq>1, freq<100)
-    _, ax = plt.subplots(figsize=FIGSIZE)
-    plot_spectra_2conditions(psd_pre[:, f_mask], psd_post[:, f_mask], 
-                             freq[f_mask], shade_sem=True, color=colors,
-                             title=title, fname=f"{dir}/{fname}", ax=ax)
+    for ax, material, color in zip(axes, MATERIALS, ['brown', 'blue']):
+        # load data
+        fname = f"{PROJECT_PATH}/data/ieeg_spectral_results/psd_{material}_hit_XXXstim.npz"
+        data_pre = np.load(fname.replace("XXX", "pre"))
+        data_post = np.load(fname.replace("XXX", "post"))
+        psd_pre = data_pre['spectra'][stats[f"sig_{feature}"]]
+        psd_post = data_post['spectra'][stats[f"sig_{feature}"]]
+        freq = data_pre['freq']
+
+        # plot
+        title = f"{material[0].upper()}{material[1:-1]}-encoding"
+        colors = [COLORS[f'light_{color}'], COLORS[color]]
+        f_mask = np.logical_and(freq>FREQ_RANGE[0], freq<FREQ_RANGE[1])
+        plot_spectra_2conditions(psd_pre[:, f_mask], psd_post[:, f_mask], 
+                                freq[f_mask], shade_sem=True, title=title, 
+                                ax=ax, color=colors)
+        
+    # save
+    plt.savefig(f"{dir}/mean_spectra_sig_{feature}.png")
+    plt.close()
 
         
 if __name__ == "__main__":
