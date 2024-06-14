d9369de7a815373a4e4ce2c8362a8f35e10e359b
Michael <mpreston@ucsd.edu>
2024-06-13 18:23:18 -0700

    format figure

--- scripts/plotting/plot_intersection_frequency.py
+++ scripts/plotting/plot_intersection_frequency.py
@@ -18,11 +18,11 @@ sys.path.append("code")
 from paths import PROJECT_PATH
 from utils import get_start_time, print_time_elapsed
 from info import MATERIALS
-from settings import COLORS
+from settings import COLORS, FREQ_RANGE
 from plots import plot_spectra_2conditions
 
 # settings
-FIGSIZE = [5, 2]
+FIGSIZE = [4, 2]
 
 
 def main():
@@ -65,9 +65,14 @@ def main():
 
     # plot spectra
     plot_spectra_2conditions(spectra_pre, spectra_post, freq, ax=ax1,
-                                color=[COLORS['light_brown'], COLORS['brown']])
+                                color=[COLORS['light_blue'], COLORS['blue']],
+                                shade_sem=False)
     ax1.set_title('Grand average')
-    ax1.set_xlim([4, 100]) # SpecParam fitting range
+    ax1.set_xlim(FREQ_RANGE)
+
+    # shrink legend textfontsize
+    for t in ax1.get_legend().get_texts():
+        t.set_fontsize(8)
 
     # annotate intersection frequency
     median_f = np.nanmedian(f_intersection)
@@ -76,14 +81,15 @@ def main():
                 color='k', s=20, zorder=10)
 
     # plot histogram
-    ax2.set_title('Baseline v. encoding')
-    bin_edges = np.linspace(0, 100, 11)
-    ax2.hist(f_intersection, bins=bin_edges, color=COLORS['brown'])
-    ax2.set_xlabel('intersection frequency (Hz)')
+    ax2.set_title('Intersection')
+    # bin_edges = np.linspace(0, 100, 11)
+    # ax2.hist(f_intersection, bins=bin_edges, color=COLORS['brown'])
+    ax2.hist(f_intersection, color=COLORS['blue'])
+    ax2.set_xlabel('frequency (Hz)')
     ax2.set_ylabel('electrode count')
     ax2.axvline(np.nanmedian(f_intersection), color='k', linestyle='--')
-    ax2.text(0.7, 0.9, f"median={int(np.nanmedian(f_intersection))} Hz", 
-            transform=ax2.transAxes, ha='center', va='center')
+    # ax2.text(0.7, 0.9, f"median={int(np.nanmedian(f_intersection))} Hz", 
+    #         transform=ax2.transAxes, ha='center', va='center')
 
     # save fig
     fig.savefig(f"{dir_output}/intersection_frequency.png")
