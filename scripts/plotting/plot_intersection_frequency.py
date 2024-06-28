79b5b243fef17f191aa4c05371363a57b69c319f
Michael <mpreston@ucsd.edu>
2024-06-28 10:48:50 -0700

    add simulation subplot

--- scripts/plotting/plot_intersection_frequency.py
+++ scripts/plotting/plot_intersection_frequency.py
@@ -1,8 +1,9 @@
 """
 Plot intersection frequency results. 
 
-Subplot A: grand average power spectra.
-Subplot B: histogram of intersection frequency, for each condition.
+Subplot A: simulation of the effects of spectral rotation on total band power.
+Subplot B: grand average power spectra.
+Subplot C: histogram of intersection frequency.
 """
 
 # Imports - standard
@@ -10,6 +11,8 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from neurodsp.spectral import rotate_powerlaw
+from specparam.sim import sim_power_spectrum
 
 # Imports - custom
 import sys
@@ -17,11 +20,14 @@ sys.path.append("code")
 from paths import PROJECT_PATH
 from utils import get_start_time, print_time_elapsed
 from info import MATERIALS
-from settings import FREQ_RANGE, RGB, WIDTH
+from settings import FREQ_RANGE, RGB, WIDTH, BANDS, BCOLORS
 from plots import plot_spectra_2conditions, beautify_ax
+from specparam_utils import compute_band_power
 
 # settings
 plt.style.use('mplstyle/default.mplstyle')
+figsize = [WIDTH['2col'], WIDTH['2col']/3]
+
 
 def main():
     # display progress
@@ -58,14 +64,16 @@ def main():
     spectra_post = np.concatenate([psd[material, 'post'] for material in MATERIALS])
 
     # create figure
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize=[WIDTH['1col'], WIDTH['1col']/2], 
-                                   constrained_layout=True)
+    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=figsize, 
+                                        constrained_layout=True)
 
-    # plot spectra
+    # plot simulation: Effects of spectral rotation on total band power
+    simulation_subplot(ax0)
+
+    # plot empirical spectra
     plot_spectra_2conditions(spectra_pre, spectra_post, freq, ax=ax1,
                                 color=['grey', 'k'],
                                 shade_sem=False)
-    ax1.set_title('Grand average')
     ax1.set_xlim(FREQ_RANGE)
     ax1.grid(False)
 
@@ -76,8 +84,7 @@ def main():
                 color=RGB[2], s=20, zorder=10)
     ax1.legend(['baseline', 'encoding', 'intersection'], loc='lower left')
 
-    # plot histogram
-    ax2.set_title('Intersection frequency')
+    # plot histogram of intersection frequency
     bins = np.linspace(0, 100, 11)
     ax2.hist(f_intersection, bins, color=RGB[2])
     ax2.set_xlabel('frequency (Hz)')
@@ -87,6 +94,11 @@ def main():
     for ax in [ax1, ax2]:
         beautify_ax(ax)
 
+    # set titles
+    ax0.set_title('Simulation:\neffects of spectral rotation')
+    ax1.set_title('Grand average power spectra')
+    ax2.set_title('Intersection frequency')
+
     # save fig
     fig.savefig(f"{dir_output}/intersection_frequency")
     fig.savefig(f"{dir_output}/intersection_frequency.png")
@@ -94,6 +106,70 @@ def main():
     # display progress
     print(f"\n\nTotal analysis time:")
     print_time_elapsed(t_start)
+
+
+def simulation_subplot(ax):
+    """ 
+    Run simulation and plot results.
+    """
+
+    # run simulation with different rotation frequencies
+    f_rotations = np.arange(5, 105, 5)
+    dfs = []
+    for f_rotation in f_rotations:
+        df = run_simulation(f_rotation)
+        df['f_rotation'] = f_rotation
+        dfs.append(df)
+    results = pd.concat(dfs, axis=0, ignore_index=True)
+
+    # plot change in power v rotation frequency
+    for band, _ in BANDS.items():
+        df = results.loc[results['band']==band]
+        df.plot(x='f_rotation', y='diff', ax=ax, color=BCOLORS[band])
+
+    # label plot
+    ax.set(xlabel='intersection frequency (Hz)', ylabel='$\Delta$ total band power')
+    ax.axhline(0, color='k')
+    handles, labels = ax.get_legend_handles_labels()
+    labels = [band for band, _ in BANDS.items()]
+    ax.legend(handles, labels)
+
+    # annotate each bands
+    for band, f_range in BANDS.items():
+        # shade band
+        ax.axvspan(f_range[0], f_range[1], color=BCOLORS[band], alpha=0.2)
+
+        # mark center
+        ax.axvline(np.mean(f_range), linestyle='--', color='k', linewidth=1)
+
+
+def run_simulation(f_rotation, delta_exponent=1, aperiodic_params=[10, 2],
+                    f_range=[4, 100]):
+    """ 
+    Simulate power spectrum and rotate it. Compute band power before and 
+    after rotation.
+    """
+
+    # simulate power spectrum and rotate
+    freqs, psd_pre = sim_power_spectrum(f_range, aperiodic_params, [])
+    psd_post = rotate_powerlaw(freqs, psd_pre, delta_exponent, f_rotation)
+    spectra = np.vstack([psd_pre, psd_post])
+
+    # compute band power
+    df_list = []
+    for band, f_range in BANDS.items():
+        power = compute_band_power(freqs, spectra, f_range, method='mean', log_power=True)
+        for i_epoch, epoch in enumerate(['pre', 'post']):
+            df_i = pd.DataFrame({'epoch': epoch, 'band': band, 
+                                'power': power[i_epoch]}, index=[0])
+            df_list.append(df_i)
+    df = pd.concat(df_list, axis=0)
+
+    # compute change in power
+    results = df.pivot(index='band', columns='epoch', values='power').reset_index()
+    results['diff'] = results['post'] - results['pre']
+
+    return results
 
 
 if __name__ == "__main__":
