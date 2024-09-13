d540a2d0c9633a8c0599731a17a3ec6568dd82f8
Michael <mpreston@ucsd.edu>
2024-09-12 17:52:14 -0700

    split B by band

--- scripts/figures/figure_3cdef.py
+++ scripts/figures/figure_3cdef.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.colors import ListedColormap
 from nilearn import plotting
 
 # Imports - custom
@@ -47,17 +48,20 @@ def main():
     # create figure and gridspec
     fig = plt.figure(figsize=(WIDTH['2col'], WIDTH['2col']/4), 
                      constrained_layout=True)
-    spec = gridspec.GridSpec(figure=fig, ncols=4, nrows=1,
+    spec = gridspec.GridSpec(figure=fig, ncols=4, nrows=2,
                             width_ratios=[0.75, 2.75, 1, 1])
-    axa = fig.add_subplot(spec[0,0])
-    axb = fig.add_subplot(spec[0,1])
-    axc = fig.add_subplot(spec[0,2])
-    axd = fig.add_subplot(spec[0,3])
+    axa = fig.add_subplot(spec[:,0])
+    axbu = fig.add_subplot(spec[0,1])
+    axbl = fig.add_subplot(spec[1,1])
+    axc = fig.add_subplot(spec[:,2])
+    axd = fig.add_subplot(spec[:,3])
 
     # shift subplot spaceing (nilearn plot including inexplicable whitespace)
-    boxb = axb.get_position()
-    axb.set_position([boxb.x0 - 0.04, boxb.y0, boxb.width, boxb.height])
-    
+    boxb = axbu.get_position()
+    axbu.set_position([boxb.x0 - 0.04, boxb.y0, boxb.width, boxb.height])
+    boxb = axbl.get_position()
+    axbl.set_position([boxb.x0 - 0.04, boxb.y0, boxb.width, boxb.height])
+
     # plot barchart: number of task-modulated electrodes
     x = [0, 1, 2]
     y = [df[col].sum() / len(df) * 100 for col in ['sig_alpha', 'sig_gamma', 'sig_all']] 
@@ -69,27 +73,24 @@ def main():
     beautify_ax(axa)
     
     # plot glass brain: electrode locations
-    df_ = df[df['sig_all']]
-    nfig = plotting.plot_markers(node_coords=df_[['pos_x', 'pos_y', 'pos_z']].values, 
-                        node_values=df_['sig_all'], node_size=2, colorbar=False, 
-                        node_cmap='binary', node_vmin=0, node_vmax=1, 
-                        display_mode='ortho', axes=axb, annotate=False)
-    nfig.annotate(size=7) # must plot with annotate=False, then set size here
-
-    df_alpha = df[(df['sig_alpha']) & (~df['sig_all'])]
-    nfig.add_markers(marker_coords=df_alpha[['pos_x', 'pos_y', 'pos_z']].values, 
-                     marker_size=1, marker_color=BCOLORS['alpha'])
-    
-    df_gamma = df[(df['sig_gamma']) & (~df['sig_all'])]
-    nfig.add_markers(marker_coords=df_gamma[['pos_x', 'pos_y', 'pos_z']].values,
-                        marker_size=1, marker_color=BCOLORS['gamma'])
+    for ax, band in zip([axbu, axbl], ['alpha', 'gamma']):
+        # shift subplot spaceing (nilearn plot including inexplicable whitespace)
+        coords = df.loc[df[f'sig_{band}'], ['pos_x', 'pos_y', 'pos_z']].values
+        nfig = plotting.plot_markers(axes=ax, node_coords=coords, 
+                                     node_values=np.ones(len(coords)), 
+                                     node_cmap=ListedColormap([BCOLORS[band]]),
+                                     display_mode='ortho', node_size=1,
+                                     colorbar=False, annotate=False)
+        coords = df.loc[df[f'sig_all'], ['pos_x', 'pos_y', 'pos_z']].values
+        nfig.add_markers(marker_coords=coords, marker_size=1, marker_color='k')
+        nfig.annotate(size=7) # must plot with annotate=False, then set size here
 
     # plot spectra: group mean for word and face blocks
     plot_group_spectra(df, [axc, axd])
 
     # set titles
     axa.set_title("Task-modulated\nelectrode counts")
-    axb.set_title("Task-modulated electrode locations")
+    axbu.set_title("Task-modulated electrode locations")
     axc.set_title("\nword-encoding block")
     axd.set_title("\nface-encoding block")
     fig.text(0.8, 0.97, "Mean power spectra", ha='center', va='center',
