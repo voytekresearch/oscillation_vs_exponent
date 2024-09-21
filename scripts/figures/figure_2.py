50b04ee624a7ca6c527914358e10f3870b71fcfc
Michael <mpreston@ucsd.edu>
2024-09-20 19:41:01 -0700

    update colors to match

--- scripts/figures/figure_2.py
+++ scripts/figures/figure_2.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from nilearn import plotting
 
 # Imports - custom
@@ -41,20 +42,28 @@ def main():
     _, (ax0, ax1) = plt.subplots(1, 2, figsize=(WIDTH['2col'], WIDTH['2col']/4), 
                                width_ratios=[1, 3], constrained_layout=True)
 
-    # plot electrode locations - color each patient
-    df['patient_color'] = df['patient'].map({p: i/len(PATIENTS) for i, p in enumerate(PATIENTS)})
-    nfig = plotting.plot_markers(node_coords=df[['pos_x', 'pos_y', 'pos_z']].values, 
-                        node_values=df['patient_color'], node_size=2, 
-                        node_cmap='tab20', colorbar=False, display_mode='ortho', 
-                        axes=ax1, annotate=False)
-    nfig.annotate(size=7) # must plot with annotate=False, then set size here
-
-    # plot bargraph of number of electrodes per patient - match colors to 'tab20' above
+    # plot bargraph of number of electrodes per patient
     colors = plt.cm.tab20.colors[:len(PATIENTS)]
     df['patient_idx'] = df['patient'].map({p: i + 1 for i, p in enumerate(PATIENTS)})
     df.groupby('patient_idx').size().plot(kind='bar', ax=ax0, color=colors, width=0.8)
     ax0.set(xlabel="patient index", ylabel="number of electrodes")
     beautify_ax(ax0)
+
+    # plot electrode locations - match colors to bar graph
+    coords = df[['pos_x', 'pos_y', 'pos_z']].values
+    nfig = plotting.plot_markers(axes=ax1, node_coords=coords, node_size=1, 
+                          node_values=df['patient_idx'],
+                            display_mode='ortho', colorbar=False,
+                            annotate=False, alpha=1,
+                            node_cmap=mpl.colors.ListedColormap(colors))
+    
+    # remove gyri/sulci lines
+    for xyz in nfig.axes:
+        for axx in nfig.axes[xyz].ax.get_children():
+            if type(axx) == mpl.patches.PathPatch:
+                if axx.get_edgecolor()[0] == 0.6509803921568628:
+                    axx.remove()
+    nfig.annotate(size=7) # must plot with annotate=False, then set size here
 
     # save/show
     plt.savefig(f"{dir_output}/figure_2bc")
