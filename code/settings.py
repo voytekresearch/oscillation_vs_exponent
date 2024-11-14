961c091d1defda98648826c960d83c03c08a95e3
Michael <mpreston@ucsd.edu>
2024-11-14 10:24:20 -0800

    replce exponent pink with orange

--- code/settings.py
+++ code/settings.py
@@ -28,7 +28,8 @@ SPEC_PARAM_SETTINGS = {
     'peak_width_limits' :   [2, np.inf], # default : (0.5, 12.0) - recommends at least frequency resolution * 2
     'min_peak_height'   :   0, # default : 0
     'max_n_peaks'       :   4, # default : inf
-    'peak_threshold'    :   3} # default : 2.0
+    'peak_threshold'    :   3 # default : 2.0
+}
 AP_MODE = 'knee'
 
 # Plotting
@@ -40,23 +41,25 @@ COLORS = {
 }
 
 BCOLORS = {
-    "alpha"     :   np.array([117,112,179]) / 255,
-    "gamma"     :   np.array([102,166,30]) / 255,
-    "exponent"  :   np.array([231,41,138]) / 255,
+    "alpha"     :   np.array([117,112,179]) / 255, # purple
+    "gamma"     :   np.array([102,166,30]) / 255, # green
+    "exponent"  :   np.array([217,95,2]) / 255, # orange
 }
 
 MCOLORS = {
-    "words"         :   np.array([166,97,26]) / 255,
-    "faces"          :   np.array([1,133,113]) / 255,
+    "words" :   np.array([166,97,26]) / 255, # brown
+    "faces" :   np.array([1,133,113]) / 255, # blue
 }
 
-RGB = (np.array([228,26,28]) / 255,
-       np.array([77,175,74]) / 255,
-       np.array([55,126,184]) / 255)
+RGB = (
+    np.array([228,26,28]) / 255,
+    np.array([77,175,74]) / 255,
+    np.array([55,126,184]) / 255
+)
 
 WIDTH = {
-    "1col"    :    3.46457, # 88mm
-    "2col"    :    7.08661, # 180mm
+    "1col"  :   3.46457, # 88mm
+    "2col"  :   7.08661, # 180mm
 }
 
 PANEL_FONTSIZE = 9
