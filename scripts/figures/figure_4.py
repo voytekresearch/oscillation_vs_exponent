543e5a965834fae8d6e9b9062caddb416441b961
Michael <mpreston@ucsd.edu>
2024-11-14 18:03:16 -0800

    combine materials. add total power

--- scripts/figures/figure_4.py
+++ scripts/figures/figure_4.py
@@ -1,9 +1,9 @@
 """
 Plot results of scripts/step4_spectral_parameterization.py. For each feature of
 interest, plot the distribution of values for the baseline and encoding period
-as a violin and swarm plot, with 'split' and 'dodge' set to True. Also plot the
-distribution of change in each feature between the baseline and encoding periods
-as a histogram below the violin plot. 
+as a violin (and swarm plot), with 'split' and 'dodge' set to True. Also plot
+the distribution of change in each feature between the baseline and encoding 
+periods as a histogram below the violin plot. 
 
 """
 
@@ -11,9 +11,10 @@ as a histogram below the violin plot.
 import os
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.collections import PolyCollection
+from matplotlib.patches import Patch
 import seaborn as sns
 from statsmodels.stats.multitest import multipletests
 
@@ -23,7 +24,6 @@ sys.path.append("code")
 from paths import PROJECT_PATH
 from info import MATERIALS
 from utils import get_start_time, print_time_elapsed
-from plots import set_custom_colors
 from settings import WIDTH, BANDS, PANEL_FONTSIZE
 
 # settings
@@ -31,9 +31,19 @@ plt.style.use('mplstyle/nature_neuro.mplstyle')
 ALPHA = 0.05 # significance level
 PLOT_SWARM = False # plot swarm plot on top of violin plot
 
-FEATURES = ['exponent', 'alpha_adj', 'gamma_adj']
-TITLES = ['Aperiodic exponent', 'Adjusted alpha power', 'Adjusted gamma power']
-LABELS = ['exponent', 'power (\u03BCV\u00b2/Hz)', 'power (\u03BCV\u00b2/Hz)']
+FEATURES = ['alpha', 'gamma', 'exponent', 'alpha_adj', 'gamma_adj']
+TITLES = ['Total alpha power', 'Total gamma power', 'Aperiodic exponent', 
+          'Adjusted alpha power', 'Adjusted gamma power']
+
+LABELS = ['power (\u03BCV\u00b2/Hz)', 'power (\u03BCV\u00b2/Hz)', 'exponent', 
+          'power (\u03BCV\u00b2/Hz)', 'power (\u03BCV\u00b2/Hz)']
+
+COLORS = [
+    np.array([223,194,125]) / 255,
+    np.array([166,97,26]) / 255,
+    np.array([128,205,193]) / 255,
+    np.array([1,133,113]) / 255,
+]
 
 
 def main():
@@ -47,7 +57,6 @@ def main():
         os.makedirs(f"{dir_output}")
 
     ### load data ##############################################################
-
     # load SpecParam results
     fname = f"{PROJECT_PATH}/data/results/spectral_parameters.csv"
     df = pd.read_csv(fname, index_col=0)
@@ -63,76 +72,53 @@ def main():
                     on=['patient', 'chan_idx', 'material', 'memory'])
     for df in [df_w, df_f]: # compute joint significance
         df['sig_all'] = df[[f'{band}_sig' for band in BANDS]].all(axis=1)
+    df = pd.concat([df_w, df_f], ignore_index=True)
+    df = df.loc[df['sig_all']].reset_index(drop=True)
 
     # load group-level statistical results
     fname = f"{PROJECT_PATH}/data/ieeg_stats/group_level_hierarchical_bootstrap_active.csv"
     stats = pd.read_csv(fname, index_col=0)
-    stats = stats.loc[stats['memory'] =='hit'].reset_index(drop=True)
+    stats = stats.loc[stats['memory'] =='hit'].reset_index(drop=True) # only successful memory trials
     stats['p'] = stats['pvalue'].apply(lambda x: min(x, 1-x)) # standardize p-values (currently 0.5 represents equal and <0.05 and >0.95 are both significant)
-    stats.loc[stats['p']==0, 'p'] = 0.001 # set p=0 to p=0.001
-    mt = multipletests(stats['p'], alpha=0.05, method='holm')
+    stats.loc[stats['p']==0, 'p'] = 0.001 # set p=0 to p=0.001 (lowest possible value with 1000 bootstrap samples)
+    mt = multipletests(stats['p'], alpha=0.05, method='holm') # multiple comparisons correction
     stats['p_corr'] = mt[1]
     
     ### plot ##################################################################
-
     # create figure and gridspec
-    fig = plt.figure(figsize=[WIDTH['2col'], WIDTH['2col']/2], 
-                     constrained_layout=True)
-    spec = gridspec.GridSpec(figure=fig, ncols=6, nrows=2, 
-                             height_ratios=[2.5,1])
+    fig = plt.figure(figsize=[WIDTH['2col'], WIDTH['2col']], 
+                     tight_layout=True)
+    spec = gridspec.GridSpec(figure=fig, ncols=3, nrows=2)
 
     # loop features
-    x_positions = [0.17, 0.5, 0.85]
-    for i_feature, (feature, label, title, xpos) in \
-        enumerate(zip(FEATURES, LABELS, TITLES, x_positions)):
+    for i_feature, (feature, label, title) in \
+        enumerate(zip(FEATURES, LABELS, TITLES)):
 
-        # add feature label above pairs of subplots
-        fig.text(xpos, 0.975, title, ha='center', va='center', fontsize=7)
+        # create nested gridspec for each feature
+        gs = gridspec.GridSpecFromSubplotSpec(2, 2, height_ratios=[2, 0.8],
+                                              subplot_spec=spec[i_feature+1])
 
-        # loop materials
-        for i_material, (material, df, plot_color) in \
-            enumerate(zip(MATERIALS, [df_w, df_f], ['browns', 'blues'])):
+        # plot violin
+        ax_v = fig.add_subplot(gs[0, :])
+        ax_v.set_title(title)
+        plot_violin(ax_v, df, feature, label)
 
-            # get sig chans only
-            df = df.loc[df['sig_all']].reset_index(drop=True)
+        # plot histogram
+        ax_h0 = fig.add_subplot(gs[1, 0])
+        ax_h1 = fig.add_subplot(gs[1, 1], sharey=ax_h0)
+        ax_h0.set_ylabel('channel count')
+        plot_histogram([ax_h0, ax_h1], df, feature, label)
 
-            # create subplots
-            ax_v = fig.add_subplot(spec[0, i_feature*2+i_material])
-            ax_h = fig.add_subplot(spec[1, i_feature*2+i_material])
-            
-            # set plotting params
-            set_custom_colors(plot_color) # set color palette
-            plotting_params = {
-                'data'  :   df,
-                'hue'   :   'epoch',
-                'y'     :   feature,
-                'dodge' :   True,
-                'split' :   True
-            }
-
-            # plot violin
-            vp = sns.violinplot(**plotting_params, ax=ax_v)
-            for l in ax_v.lines: # remove mean and quartile line
-                l.set_linewidth(0)
-            vp.set_xlabel('')
-            vp.set_ylabel(label)
-            vp.set_title(f"\n{material[:-1]}-encoding")
-            vp.set_xticks([])
-            ax_v.legend(loc='upper left', bbox_to_anchor=(0, 0))
-
-            # plot swarm
-            if PLOT_SWARM:
-                plotting_params.pop('split')
-                sns.swarmplot(**plotting_params, ax=ax_v, size=0.5, 
-                                palette='dark:#000000')
-                
-            # plot histogram
-            plot_histogram(ax_h, ax_v, df, feature, label, stats, material)
+        # annotate stats
+        print_stats([ax_h0, ax_h1], stats, feature)
 
     # add figure panel labels
-    fig.text(0.02, 0.98, 'a', fontsize=PANEL_FONTSIZE, fontweight='bold')
-    fig.text(0.34, 0.98, 'b', fontsize=PANEL_FONTSIZE, fontweight='bold')
-    fig.text(0.68, 0.98, 'c', fontsize=PANEL_FONTSIZE, fontweight='bold')
+    # fig.text(0.02, 0.98, 'a', fontsize=PANEL_FONTSIZE, fontweight='bold')
+    fig.text(0.34, 0.98, 'a', fontsize=PANEL_FONTSIZE, fontweight='bold')
+    fig.text(0.68, 0.98, 'b', fontsize=PANEL_FONTSIZE, fontweight='bold')
+    fig.text(0.02, 0.48, 'c', fontsize=PANEL_FONTSIZE, fontweight='bold')
+    fig.text(0.34, 0.48, 'd', fontsize=PANEL_FONTSIZE, fontweight='bold')
+    fig.text(0.68, 0.48, 'e', fontsize=PANEL_FONTSIZE, fontweight='bold')
 
     # save
     fig.savefig(f"{dir_output}/figure_4", bbox_inches='tight')
@@ -143,32 +129,85 @@ def main():
     print_time_elapsed(t_start)
 
 
-def plot_histogram(ax, ax_v, df, feature, label, stats, material):
-    # compute difference
+def plot_violin(ax, df, feature, label):
+    plotting_params = {
+        'data'  :   df,
+        'x'     :   'material',
+        'y'     :   feature,
+        'hue'   :   'epoch',
+        'dodge' :   True,
+        'split' :   True
+    }
+        
+    # plot violin
+    vp = sns.violinplot(**plotting_params, ax=ax)
+    for l in ax.lines: # remove mean and quartile line
+        l.set_linewidth(0)
+
+    # set labels
+    vp.set_ylabel(label)
+    vp.set_xlabel('')
+    vp.set_xticks([0, 1], ['word-encoding', 'face-encoding'], fontsize=6)
+
+    # re-set color of violin plot
+    iv = 0
+    for violin in vp.collections:
+        if isinstance(violin, PolyCollection):
+            violin.set_facecolor(COLORS[iv])
+            iv += 1
+
+    # create legend
+    legend_elements = [
+        Patch(facecolor=COLORS[0], edgecolor='black', label='word, baseline'),
+        Patch(facecolor=COLORS[1], edgecolor='black', label='word, encoding'),
+        Patch(facecolor=COLORS[2], edgecolor='black', label='face, baseline'),
+        Patch(facecolor=COLORS[3], edgecolor='black', label='face, encoding')
+    ]
+    ax.legend(handles=legend_elements, loc='upper center')
+
+    # plot swarm
+    if PLOT_SWARM:
+        plotting_params.pop('split')
+        sns.swarmplot(**plotting_params, ax=ax, size=0.5, 
+                        palette='dark:#000000')
+
+
+def plot_histogram(axes, df, feature, label):
+    # compute difference between encoding and baseline
     df_p = df.pivot_table(index=['patient', 'chan_idx', 'material'], 
                               columns='epoch', values=feature).reset_index()
     df_p['diff'] = df_p['post'] - df_p['pre']
 
     # plot
-    max_val = np.nanmax(np.abs(df_p['diff']))
-    bins = np.linspace(-max_val, max_val, 21)
-    ax.hist(df_p['diff'], bins=bins, color='grey')
-    ax.set(xlim=[-max_val, max_val])
-    ax.set_xlabel(f"$\Delta$ {label}")
-    # ax.set_ylabel('channel count')
-    ax.axvline(0, color='k', linestyle='-', linewidth=0.5)
-    ax.axvline(np.nanmean(df_p['diff']), color='r', linewidth=0.5)
+    for material, ax in zip(MATERIALS, axes):
+        df_m = df_p.loc[df_p['material']==material]
+        max_val = np.nanmax(np.abs(df_m['diff']))
+        bins = np.linspace(-max_val, max_val, 21)
+        ax.hist(df_m['diff'], bins=bins, color='grey')
+        ax.set(xlim=[-max_val, max_val])
+        ax.set_xlabel(f"$\Delta$ {label}")
+        ax.axvline(0, color='k', linestyle='-', linewidth=0.5)
+        ax.axvline(np.nanmean(df_m['diff']), color='r', linewidth=0.5)
 
-    # add stats
-    pval = stats.loc[((stats['material']==material) & 
-                (stats['feature']==feature)), 'p_corr'].values
-    pos = [0.6, -0.1]
-    if pval < 0.001:
-        ax_v.text(*pos, f"$*p<0.001", transform=ax_v.transAxes)
-    elif pval < ALPHA:
-        ax_v.text(*pos, f"*p={pval[0]:.3f}", transform=ax_v.transAxes)
-    else:
-        ax_v.text(*pos, f"p={pval[0]:.3f}", transform=ax_v.transAxes)
+
+def print_stats(axes, stats, feature):
+    for material, ax in zip(MATERIALS, axes):
+        pval = stats.loc[((stats['material']==material) & 
+                    (stats['feature']==feature)), 'p_corr'].values[0]
+        sign = np.sign(stats.loc[((stats['material']==material) & 
+                                  (stats['feature']==feature)), 
+                                  'cohens_d'].values[0])
+        if sign >= 0:
+            pos = [0.58, 0.86]
+        else:
+            pos = [0.04, 0.86]
+        if pval < 0.001:
+            ax.text(*pos, f"p<0.001*", transform=ax.transAxes)
+        elif pval < ALPHA:
+            ax.text(*pos, f"p={pval:.3f}*", transform=ax.transAxes)
+        else:
+            ax.text(*pos, f"p={pval:.3f}", transform=ax.transAxes)
+
 
 if __name__ == "__main__":
     main()
